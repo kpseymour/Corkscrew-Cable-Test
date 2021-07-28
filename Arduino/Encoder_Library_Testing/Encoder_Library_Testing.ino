77d666ee4b35015b631b4b9b66986f7d405d2ea2
Kyle <31325210+kpseymour@users.noreply.github.com>
2021-07-28 17:17:49 -0400

    Add files via upload

--- Arduino/Encoder_Library_Testing/Encoder_Library_Testing.ino
+++ Arduino/Encoder_Library_Testing/Encoder_Library_Testing.ino
@@ -3,6 +3,29 @@
 #include "DualG2HighPowerMotorShield.h"
 DualG2HighPowerMotorShield18v18 md;
 
+
+//-------------------------------------------------------------------------------------------------//
+//   _____         ______ ______ _________     __
+//  / ____|  /\   |  ____|  ____|__   __\ \   / /
+// | (___   /  \  | |__  | |__     | |   \ \_/ / 
+//  \___ \ / /\ \ |  __| |  __|    | |    \   /  
+//  ____) / ____ \| |    | |____   | |     | |   
+// |_____/_/    \_\_|    |______|  |_|     |_| 
+
+
+const int elevatorBottomPin = 8;
+const int elevatorTopPin = 12;
+const int ledPin = 13;
+int ledState = LOW;
+int bottomButtonState, topButtonState;
+int lastBottonButtonState = LOW;
+int lastTopButtonState = LOW;
+unsigned long lastDebounceTime = 0; 
+unsigned long debounceDelay = 50;  
+
+
+
+// Motor Driver Protection code
 void stopIfFault()
 {
   if (md.getM1Fault())
@@ -21,6 +44,13 @@ void stopIfFault()
   }
 }
 
+//-------------------------------------------------------------------------------------------------//
+//  __  __  ____ _______ ____  _____   _____ 
+// |  \/  |/ __ \__   __/ __ \|  __ \ / ____|
+// | \  / | |  | | | | | |  | | |__) | (___  
+// | |\/| | |  | | | | | |  | |  _  / \___ \ 
+// | |  | | |__| | | | | |__| | | \ \ ____) |
+// |_|  |_|\____/  |_|  \____/|_|  \_\_____/ 
 /* Encoder Pins
  * pins 2 and 3 have interrupt capability on Arduino UNO
  * Best Performance: both pins have interrupt capability 
@@ -28,12 +58,16 @@ void stopIfFault()
  * Low Performance:  neither pin has interrupt capability */
 Encoder encoderTwister(2, 6);
 Encoder encoderElevator(3, 7);
+long positionTwister  = -999;
+long positionElevator = -999;
 
 // set the number of encoder ticks translates to 1 rotation.
 int rotationTicks = 200; 
 
 // set the number of encoder ticks translates to 1 inch of elevator movement
 int inchTicks = 75;
+
+
 
 //-------------------------------------------------------------------------------------------------//
 //  _____ _____ _____  
@@ -44,11 +78,11 @@ int inchTicks = 75;
 // |_|   |_____|_____/ 
 
 // Set up the PID variables for Twister here:
-double KpT=5, KiT=.3, KdT=1;
+double KpT=1, KiT=0, KdT=.01;
 double twisterPIDInput, twisterPIDOutput, twisterPIDSetpoint;
 
 // Set up the PID variables for Elevator here:
-double KpE=5, KiE=.3, KdE=1;
+double KpE=1, KiE=0, KdE=.01;
 double elevatorPIDInput, elevatorPIDOutput, elevatorPIDSetpoint;
 
 PID twisterPID(&twisterPIDInput, &twisterPIDOutput, &twisterPIDSetpoint, KpT, KiT, KdT, DIRECT);
@@ -56,9 +90,20 @@ PID elevatorPID(&elevatorPIDInput, &elevatorPIDOutput, &elevatorPIDSetpoint, KpE
 
 
 //-------------------------------------------------------------------------------------------------//
-
-
+//   _____ ______ _______ _    _ _____  
+//  / ____|  ____|__   __| |  | |  __ \ 
+// | (___ | |__     | |  | |  | | |__) |
+//  \___ \|  __|    | |  | |  | |  ___/ 
+//  ____) | |____   | |  | |__| | |     
+// |_____/|______|  |_|   \____/|_|    
+ 
 void setup() {
+  pinMode(elevatorBottomPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+
+  // set initial LED state
+  digitalWrite(ledPin, ledState);
+  
   Serial.begin(9600);
   Serial.println("Elevator Motors Encoder Test:");
   twisterPIDInput = 0;
@@ -71,10 +116,41 @@ void setup() {
   elevatorPID.SetMode(AUTOMATIC);
 }
 
-long positionTwister  = -999;
-long positionElevator = -999;
+
+//-------------------------------------------------------------------------------------------------//
+//  _      ____   ____  _____  
+// | |    / __ \ / __ \|  __ \ 
+// | |   | |  | | |  | | |__) |
+// | |   | |  | | |  | |  ___/ 
+// | |___| |__| | |__| | |     
+// |______\____/ \____/|_|  
 
 void loop() {
+
+  // check to see if a stop switch has been hit
+  int readingBottom = digitalRead(elevatorBottomPin);
+  int readingTop = digitalRead(elevatorTopPin);
+  if (readingBottom != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (readingBottom != buttonState) {
+      buttonState = readingBottom;
+      if (buttonState == HIGH) {
+        ledState = !ledState;
+      }
+    }
+  }
+  // Change the LED
+  digitalWrite(ledPin, ledState);
+  lastButtonState = readingBottom;
+
+
+//-------------------------------------------------------------------------------------------------//
+
+  
   long newTwister, newElevator;
   newTwister = encoderTwister.read();
   newElevator = encoderElevator.read();
